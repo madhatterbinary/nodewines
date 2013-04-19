fb2b0d231e7503cf6bd52f28a76cf79bcca7139a
madhatterbinary <ulysses@mojitocoder.com>
2013-04-19 20:30:52 +0100

    wines

--- routes/wines.js
+++ routes/wines.js
@@ -4,7 +4,7 @@ var Server = mongo.Server,
     Db = mongo.Db,
     BSON = mongo.BSONPure;
 
-var server = new Server('localhost', 27017, {auto_reconnect: true});
+var server = new Server('localhost', 9404, {auto_reconnect: true});
 db = new Db('winedb', server, {safe: true});
  console.log("FUCK2" + db + server);
 db.open(function(err, db) {
