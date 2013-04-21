b03845c5b6c10876b307177aa0de3b9549dd2b47
madhatterbinary <ulysses@mojitocoder.com>
2013-04-21 17:15:29 +0100

    ss

--- routes/wines.js
+++ routes/wines.js
@@ -131,7 +131,7 @@ db.Db.connect(MONGOHQ_URL, function(error, client) {
 exports.findById = function(req, res) {
     var id = req.params.id;
     console.log('Retrieving wine: ' + id);
-    db.Collection('wines', function(err, collection) {
+    db.collection('wines', function(err, collection) {
         collection.findOne({'_id':new BSON.ObjectID(id)}, function(err, item) {
             res.send(item);
         });
@@ -139,7 +139,7 @@ exports.findById = function(req, res) {
 };
 
 exports.findAll = function(req, res) {
-    db.Collection('wines', function(err, collection) {
+    db.collectionNames('wines', function(err, collection) {
         collection.find().toArray(function(err, items) {
             res.send(items);
         });
@@ -149,7 +149,7 @@ exports.findAll = function(req, res) {
 exports.addWine = function(req, res) {
     var wine = req.body;
     console.log('Adding wine: ' + JSON.stringify(wine));
-    db.Collection('wines', function(err, collection) {
+    db.collection('wines', function(err, collection) {
         collection.insert(wine, {safe:true}, function(err, result) {
             if (err) {
                 res.send({'error':'An error has occurred'});
@@ -166,7 +166,7 @@ exports.updateWine = function(req, res) {
     var wine = req.body;
     console.log('Updating wine: ' + id);
     console.log(JSON.stringify(wine));
-    db.Collection('wines', function(err, collection) {
+    db.collection('wines', function(err, collection) {
         collection.update({'_id':new BSON.ObjectID(id)}, wine, {safe:true}, function(err, result) {
             if (err) {
                 console.log('Error updating wine: ' + err);
@@ -182,7 +182,7 @@ exports.updateWine = function(req, res) {
 exports.deleteWine = function(req, res) {
     var id = req.params.id;
     console.log('Deleting wine: ' + id);
-    db.Collection('wines', function(err, collection) {
+    db.collection('wines', function(err, collection) {
         collection.remove({'_id':new BSON.ObjectID(id)}, {safe:true}, function(err, result) {
             if (err) {
                 res.send({'error':'An error has occurred - ' + err});
