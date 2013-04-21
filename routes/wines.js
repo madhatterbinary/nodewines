b05564627193941615149c895c1204ec486f825e
madhatterbinary <ulysses@mojitocoder.com>
2013-04-21 02:30:31 +0100

    mm

--- routes/wines.js
+++ routes/wines.js
@@ -120,9 +120,9 @@ var theport = process.env.PORT || 5000;
 
 // Makes connection asynchronously.  Mongoose will queue up database
 // operations and release them when the connection is complete.
-mongoose.connect(uristring, function (err, res) {
+mongoose.connect("mongodb://ulysses:lupen333@ds043497.mongolab.com:43497/heroku_app15083406", function (err, res) {
 
-    console.log (':::::::::::::::::::::::::::000::::::::::::::::::::::::::::Succeeded connected to: ' + err);
+    console.log (':::::::::::::::::::::::::::000::::::::::::::::::::::::::::Succeeded connected to: ' + err,res);
   if (err) { 
   console.log ('ERROR connecting to: ' + uristring + '. ' + err);
   } else {
