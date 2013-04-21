f0c34422f6f777afe0cb9684a01daf29d1e275ed
madhatterbinary <ulysses@mojitocoder.com>
2013-04-21 13:53:57 +0100

    dd

--- routes/wines.js
+++ routes/wines.js
@@ -3,10 +3,7 @@ var mongo = require('mongodb');
 var Server = mongo.Server,
     Db = mongo.Db,
     BSON = mongo.BSONPure;
-
-var server = new Server('localhost', 27017, {auto_reconnect: true});
-db = new Db('winecellerdb', server);
-
+var theport = process.env.PORT || 5000;
 var http = require ('http');             // For serving a basic web page.
 var mongoose = require ("mongoose"); // The reason for this demo.
 var port = process.env.PORT || 27017;
@@ -18,9 +15,15 @@ process.env.MONGOLAB_URI ||
 process.env.MONGOHQ_URL || 
 'mongodb://localhost/HelloMongoose';
 
+
+var server = new Server('localhost', theport, {auto_reconnect: true});
+db = new Db('winecellerdb', server);
+
+
+
 // The http server will listen to an appropriate port, or default to
 // port 5000.
-var theport = process.env.PORT || 5000;
+
 
 // Makes connection asynchronously.  Mongoose will queue up database   ////
 // operations and release them when the connection is complete.
@@ -45,7 +48,7 @@ var theport = process.env.PORT || 5000;
     conn.collection('wines').find().toArray(function(err, items) {
         if (err) throw err;
          console.log ('::::::::::::::::::::::::::::99999999999:::::::::::::::::::::::::::Succeeded collection to: ' + JSON.stringify(items));
-        response.send();
+        
     });  
 
 
