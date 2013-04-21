70f75104236fccc46ca11b3ae4ba128ad676f63d
madhatterbinary <ulysses@mojitocoder.com>
2013-04-21 12:23:32 +0100

    nn

--- routes/wines.js
+++ routes/wines.js
@@ -35,6 +35,7 @@ mongoose.connect("mongodb://madhatterbinary:lupen333@ds043497.mongolab.com:43497
      console.log ('::::::::::::::::::::::::::::ressssssssssssssss:::::::::::::::::::::::::::Succeeded res to: ' + res);
    db.collection('wines', function(err, collection) {
         console.log ('::::::::::::::::::::::::::::99999999999:::::::::::::::::::::::::::Succeeded collection to: ' + collection);
+        console.log(JSON.stringify(collection));
 
     });
   }
