6d1ee74139c2131a4e69a96ac04c4d706776a6f1
madhatterbinary <ulysses@mojitocoder.com>
2013-04-22 00:36:04 +0100

    finally!!!

--- routes/wines.js
+++ routes/wines.js
@@ -1,16 +1,6 @@
-//  var mongo = require('mongodb');
-// // //console.log (':::::::::::::::::::::::::::uuuuuuuuuuuuuu::::::::::::::::::::::::::::Succeeded connected to: ');
-//  var Server = mongo.Server,
-//        Db = mongo.Db,
-//        BSON = mongo.BSONPure;
-
-// var server = new Server('localhost', 27017, {auto_reconnect: true});
 var db = require('mongodb');
 var url = require('url');
 var log = console.log;
-
-// db = new Db('winecellerdb', server, {safe:true});
-
 
 var winecollection;
 var MONGOHQ_URL="mongodb://madhatterbinary:lupen333@alex.mongohq.com:10047/app15083406";
@@ -18,59 +8,31 @@ var MONGOHQ_URL="mongodb://madhatterbinary:lupen333@alex.mongohq.com:10047/app15
 var connectionUri = url.parse(MONGOHQ_URL);
 var dbName = connectionUri.pathname.replace(/^\//, '');
 
-console.log ('::::::::::123::::::::::::::::::MONGOHQ_URL:::::::::::::::::::::::::::: ' +MONGOHQ_URL);
-console.log ('::::::::::123::::::::::::::::::connectionUri:::::::::::::::::::::::::::: ' +connectionUri);
-console.log ('::::::::::123::::::::::::::::::dbName:::::::::::::::::::::::::::: ' +dbName);
- 
 db.Db.connect(MONGOHQ_URL, function(error, client) {
   if (error) throw error;
 
   client.collectionNames(function(error, names){
     if(error) throw error;
  
-    // output all collection names
-    log("collectionNamessssss!!!!!6666666666!!!!!" + names);
-
-    log("===========");
     var lastCollection = null;
         names.forEach(function(colData){
           var colName = colData.name.replace(dbName + ".", '')
            
           lastCollection = colName;
-          log("::::::::::::::::::::::::::::ALL Collection:::::::::::::::::::::::::::::: " + lastCollection);
           if (lastCollection == "wines") {
             var collection = new db.Collection(client, lastCollection);
 
                 log("\nDocuments in " + lastCollection);
                 var documents = collection.find({}, {limit:24});
                 winecollection = new db.Collection(client, lastCollection);
-    
-              log("::::::::::::::::::::::::::::documents::::end:::::::::::::::::::::::::: " + documents);
+
                  winecollection.find().toArray(function(err, items) {
                    
                    log("::::::::::::::::::::::::::::WINE collection::::00111 JSON:::::::::::::::::::::::::: " + JSON.stringify(items));
 
                 
                  });
-                // output a count of all documents found
-                documents.count(function(error, count){
-                  log("  " + count + " documents(s) found");
-                  log("====================");
-             
-                  // output the first 5 documents
-                  documents.toArray(function(error, docs) {
-                    if(error) throw error;
-             
-                    docs.forEach(function(doc){
-
-                        //log("::::::::::::::::::::::::::::docccccccccc:::::::::::::::::::::::::::::: " + doc);
-
-                    });
-                    // close the connection
-                   // client.close();
-                  });
-                });
-
+                 client.close();
           };
         }); 
     });           
